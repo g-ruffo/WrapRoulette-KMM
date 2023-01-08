fe5000b848fb515470aa4fc86fbe25fb757ac4d1
g-ruffo <grayson@veltus.ca>
2023-01-07 21:39:47 -0800

    chore: Add and refactor FirebaseStorageUtil.kt

---
+++ androidApp/src/main/java/ca/veltus/wraproulettekmm/android/utils/FirebaseStorageUtil.kt
@@ -0,0 +1,29 @@
+package ca.veltus.wraproulettekmm.android.utils
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.crashlytics.ktx.crashlytics
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import java.util.*
+
+object FirebaseStorageUtil {
+    private val storageInstance: FirebaseStorage by lazy { FirebaseStorage.getInstance() }
+
+    private val currentUserReference: StorageReference
+        get() = storageInstance.reference.child(
+            FirebaseAuth.getInstance().uid ?: throw NullPointerException("UID is null.")
+        )
+
+    fun uploadProfilePhoto(imageBytes: ByteArray, onSuccess: (imagePath: String) -> Unit) {
+        val ref =
+            currentUserReference.child("profilePictures/${UUID.nameUUIDFromBytes(imageBytes)}")
+        ref.putBytes(imageBytes).addOnSuccessListener {
+            onSuccess(ref.path)
+        }.addOnFailureListener {
+            Firebase.crashlytics.recordException(it)
+        }
+    }
+
+    fun pathToReference(path: String) = storageInstance.getReference(path)
+}
