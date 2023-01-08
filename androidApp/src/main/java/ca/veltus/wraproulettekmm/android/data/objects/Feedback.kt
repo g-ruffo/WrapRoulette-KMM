f107360d1044b00f4b29d960f86546dda0300af9
g-ruffo <grayson@veltus.ca>
2023-01-07 21:29:13 -0800

    chore: Add and refactor Feedback.kt

---
+++ androidApp/src/main/java/ca/veltus/wraproulettekmm/android/data/objects/Feedback.kt
@@ -0,0 +1,16 @@
+package ca.veltus.wraproulettekmm.android.data.objects
+
+import androidx.annotation.Keep
+import java.util.*
+
+@Keep
+data class Feedback(
+    val message: String,
+    val date: Date,
+    val senderUid: String,
+    val senderName: String,
+    val senderEmail: String,
+    var resolved: Boolean = false
+) {
+    constructor() : this("", Date(0), "", "", "", false)
+}
