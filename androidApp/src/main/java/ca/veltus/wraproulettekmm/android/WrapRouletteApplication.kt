a3ce17e5df89008d0f5aa87c15fc92fec44d84ee
g-ruffo <grayson@veltus.ca>
2023-01-07 21:37:51 -0800

    chore: Add and refactor WrapRouletteApplication.kt

---
+++ androidApp/src/main/java/ca/veltus/wraproulettekmm/android/WrapRouletteApplication.kt
@@ -0,0 +1,7 @@
+package ca.veltus.wraproulettekmm.android
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WrapRouletteApplication : Application()
