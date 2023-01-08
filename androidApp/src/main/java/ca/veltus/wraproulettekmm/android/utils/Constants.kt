0595fc33577708147908f1e3c3b81688136bd55b
g-ruffo <grayson@veltus.ca>
2023-01-07 21:40:59 -0800

    chore: Add and refactor Constants.kt

---
+++ androidApp/src/main/java/ca/veltus/wraproulettekmm/android/utils/Constants.kt
@@ -0,0 +1,23 @@
+package ca.veltus.wraproulettekmm.android.utils
+
+object Constants {
+    const val SECOND = 1000
+    const val MINUTE = 60000
+    const val HOUR = 3600000
+    const val DAY = 86400000
+    const val YEAR = 31556926000
+    const val BID_DIALOG = 0
+    const val ADMIN_DIALOG = 1
+    const val MARGIN_DIALOG = 2
+    const val PIR_DIALOG = 3
+    const val PASSWORD_ERROR = 0
+    const val EMAIL_ERROR = 1
+    const val DEPARTMENT_ERROR = 2
+    const val NAME_ERROR = 3
+    const val POOL_NAME_ERROR = 4
+    const val POOL_DATE_ERROR = 5
+    const val POOL_START_ERROR = 6
+    const val SET_FINAL_BETS = 0
+    const val SET_BET_AMOUNT = 1
+    const val SET_MARGIN_TIME = 2
+}
